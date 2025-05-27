a08b583779d096f00dd4e4c6a224945a2d15a52e
patie01 <158181235+patie01@users.noreply.github.com>
2025-05-27 22:35:57 +0200

    commit all files

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,14 +9,12 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+ return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
   };
-
-  const handleContinueShopping = (e) => {
-   
+dispatch(removeItem(item.name));
+  const handleCheckoutShopping = (e) => {
+    alert("Functionality to be added for future reference") 
   };
-
-
 
   const handleIncrement = (item) => {
   };
