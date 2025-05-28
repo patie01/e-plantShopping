4f4ed94dceb5da4d18127922baac259bdd88b788
patie01 <158181235+patie01@users.noreply.github.com>
2025-05-28 19:29:14 +0000

    Updated App and ProductList components

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
